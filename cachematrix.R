c08f00e872ca0b6147f0454a0b816216e564154a
justkoding <online.file.library@gmail.com>
2014-08-21 02:43:13 +0900

    Filled in makeCacheMatrix() and cacheSolve()

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below functions are used to cache/retrieve a matrix and its inverse.
+## makeCacheMatrix() is responsible for caching/retrieving.
+## cacheSolve() is responsible for calculating the inverse matrix.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Returns a list of closures for setting/getting the matrix and its inverse.
+makeCacheMatrix <- function(mat = matrix()) {
+    inv <- NULL
+    
+    ## Reset the inverse matrix if any of the data in the matrix changes.
+    set <- function(x) {
+        if (!identical(mat, x)) {
+            mat <<- x
+            inv <<- NULL
+        }
+    }
+    
+    ## Get the matrix.
+    get <- function() {
+        mat
+    }
+    
+    ## Set the inverse matrix as the given argument.
+    setInverse <- function(y) {
+        inv <<- y
+    }
+    
+    ## Get the inverse matrix.
+    getInverse <- function() {
+        inv
+    }
+    
+    list(set = set, get = get, 
+         setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Returns the inverse matrix.
+cacheSolve <- function(matWrapper, ...) {
+    inv <- matWrapper$getInverse()
+    
+    ## If the inverse matrix is not cached, the inverse matrix is 
+    ## calculated and cached before it is returned.
+    if (!is.null(inv)) {
+        message("Cached data found.")
+    } else {
+        data <- matWrapper$get()
+        inv <- solve(a = data, ...)
+        matWrapper$setInverse(inv)
+    }
+    
+    inv
 }
